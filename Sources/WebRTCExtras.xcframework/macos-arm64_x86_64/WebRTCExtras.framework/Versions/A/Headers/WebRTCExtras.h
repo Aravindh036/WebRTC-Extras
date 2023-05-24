f4191750ae73d46a20722c014240bc2f934b761b
Aravindh S <aravindh.saravanan@zohocorp.com>
2023-05-24 18:56:11 +0530

    removed version in xcframework

--- Sources/WebRTCExtras.xcframework/macos-arm64_x86_64/WebRTCExtras.framework/Versions/A/Headers/WebRTCExtras.h
+++ Sources/WebRTCExtras.xcframework/macos-arm64_x86_64/WebRTCExtras.framework/Versions/A/Headers/WebRTCExtras.h
@@ -7,13 +7,3 @@
 
 #import <Foundation/Foundation.h>
 #import <WebRTCExtras/DataHelper.h>
-
-//! Project version number for WebRTCExtras.
-FOUNDATION_EXPORT double WebRTCExtrasVersionNumber;
-
-//! Project version string for WebRTCExtras.
-FOUNDATION_EXPORT const unsigned char WebRTCExtrasVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <WebRTCExtras/PublicHeader.h>
-
-
