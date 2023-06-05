965df843cd1ec722e27d3c2bf80f4a459e2c42e2
Aravindh S <aravindh.saravanan@zohocorp.com>
2023-06-05 19:05:21 +0530

    updated network status change callback

--- Sources/WebRTCExtras.xcframework/macos-arm64_x86_64/WebRTCExtras.framework/Versions/A/Headers/NetworkHelper.h
+++ Sources/WebRTCExtras.xcframework/macos-arm64_x86_64/WebRTCExtras.framework/Versions/A/Headers/NetworkHelper.h
@@ -6,9 +6,30 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <Network/Network.h>
+
+typedef NS_ENUM(NSInteger, NetworkStatusType) {
+    NetworkStatusUnknown,
+    NetworkStatusConnected,
+    NetworkStatusDisconnected
+};
+
+typedef struct {
+    BOOL isWiFi;
+    BOOL isCellular;
+    BOOL isEthernet;
+    NetworkStatusType status;
+    BOOL isExpensive;
+    BOOL hasIPv4;
+    BOOL hasIPv6;
+    BOOL hasNewDNS;
+} NetworkStatus;
+
+typedef void (^NetworkStatusChangeCallback)(NetworkStatus);
 
 @interface NetworkHelper : NSObject
 
-+ (void)observeNetworkReachability:(void (^)(BOOL))callback;
+- (void)observeNetworkReachability:(NetworkStatusChangeCallback)callback;
+- (void)stopMonitoring;
 
 @end
--- WebRTCExtras.podspec
+++ WebRTCExtras.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = 'WebRTCExtras'
-  s.version      = '0.0.9'
+  s.version      = '0.0.11'
   s.summary      = 'A Objective C library for WebRTC'
   s.homepage     = 'https://github.com/Aravindh036/WebRTC-Extras'
   s.license      = 'MIT'
