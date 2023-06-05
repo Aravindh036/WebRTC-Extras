b2d29e2d529fbd102a3dcc1c8bfc431d443b0986
Aravindh S <aravindh.saravanan@zohocorp.com>
2023-06-05 16:10:01 +0530

    updated networkhelpers

---
+++ Sources/WebRTCExtras.xcframework/ios-arm64_x86_64-simulator/WebRTCExtras.framework/Headers/NetworkHelper.h
@@ -0,0 +1,10 @@
+//
+//  NetworkHelper.h
+//  WebRTCExtras
+//
+//  Created by Aravindh on 05/06/23.
+//
+
+#import <Foundation/Foundation.h>
+
+void observeNetworkReachability(void (^callback)(BOOL));
